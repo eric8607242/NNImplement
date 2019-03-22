feea9c7e8b6cc784cbf6a84cd70c9a0b1efbe401
eric8607242 <e0928021388@gmail.com>
2019-03-22 23:31:17 +0800

    Finish rnn and README

---
+++ RNN/README.md
@@ -0,0 +1,12 @@
+# RNN
+## Introduction
+This is the simple RNN without any framework(ex. tensorflow, keras, pytorch).And the optimization is SGD.You can use the input file I provided, if you don't, you can just put any contain in the input.txt file.But this is just a simple rnn, it can afford too complex input.
+
+## Architecture
+![](./RNN_architecture.png)
+
+## Instruction
+```
+pip3 install numpy
+python3 RNN.py
+```
--- RNN/RNN.py
+++ RNN/RNN.py
@@ -1,177 +1,238 @@
 import numpy as np
 
-
 class RNN:
-    """ A RNN modal 
+    def __init__(self, hidden_size, learning_rate):
+        """Initialize the RNN model and assign None value to the variable
 
-    [public method]
-    train --- training the model to get the better weight
-    
-    """
-    def __init__(self, hidden_size,learning_rate, filename):
+        Args:
+            hidden_size(int):hidden size for RNN model
+
+        Attributes:
+            data(str):training data
+            char_to_index(dict):the mapping between relationship the each char and each index in the chars set 
+            index_to_char(dict):the mapping between relationship the each index and each char in the chars set
+            data_size(int):size of the training data
+            chars_size(int):the size of the chars set which is the total chars in training data without repeat
+            learning_rate(float):learning rate in training step
+            hidden_size(int):hidden size for RNN model
+            Wxh, Whh, Why(ndarray):the weight for training step
+            bh, by(ndarray):the bias for trainging step
+        """
         self.data = None
+        self.char_to_index = None
+        self.index_to_char = None
+        self.data_size = None
+        self.chars_size = None
 
         self.learning_rate = learning_rate
         self.hidden_size = hidden_size
-        
-        self.chars_dict = {}
-        self.index_dict = {}
-
-        self.vocab_size = None
-        self.data_size = None
-
         self.hprev = None
 
         self.Wxh, self.Whh, self.Why = None, None, None
         self.bh, self.by = None, None
 
-        self.mWxh, self.mWhh, self.mWhy = None, None, None
-        self.mbh, self.mby = None, None
-
-        self.vWxh, self.vWhh, self.vWhy = None, None, None
-        self.vbh, self.vby = None, None
-
-        self.load_data(filename)
-
     def _init_weight(self):
-        """initialize the weight to random scale
-        """
-        self.Wxh = np.random.randn(self.hidden_size, self.vocab_size)*0.01
-        self.Whh = np.random.randn(self.hidden_size, self.hidden_size)*0.01
-        self.Why = np.random.randn(self.vocab_size, self.hidden_size)*0.01
-
-        self.bh = np.zeros((self.hidden_size, 1))
-        self.by = np.zeros((self.vocab_size, 1))
+        """Initialize the weight value accrodding for the size of the data and hidden size """
+        self.Wxh, self.Whh, self.Why = (
+               np.random.rand(self.hidden_size, self.chars_size)*0.01,
+               np.random.rand(self.hidden_size, self.hidden_size)*0.01,
+               np.random.rand(self.chars_size, self.hidden_size)*0.01,
+            )
+        self.bh, self.by = (
+               np.zeros((self.hidden_size, 1)),
+               np.zeros((self.chars_size, 1))
+            )
         
-        self.mWxh, self.mWhh, self.mWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
-        self.mbh, self.mby = np.zeros_like(self.bh), np.zeros_like(self.by)
-
-        self.vWxh, self.vWhh, self.vWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
-        self.vbh, self.vby = np.zeros_like(self.bh), np.zeros_like(self.by)
-
     def load_data(self, filename):
-        """load a txt file 
+        """load the data via the path of the input data
+
+        Args:
+            filename(str):the path of the input data
         """
-        print("--load data---")
         data = open(filename, 'r').read()
 
         chars_set = list(set(data))
-        for i, ch in enumerate(chars_set):
-            self.chars_dict[ch] = i
-            self.index_dict[i] = ch
-        # self.chars_dict = {ch:i for i,ch in enumerate(chars_set)}
-        # self,index_dict = {i:ch for i,ch in enumerate(chars_set)}
-        self.data_size = len(data)
-        self.vocab_size = len(chars_set)
+        self.char_to_index = {ch:i for i,ch in enumerate(chars_set)}
+        self.index_to_char = {i:ch for i,ch in enumerate(chars_set)}
 
-        self.hprev = np.zeros((self.hidden_size, 1))
+        self.data_size = len(data)
+        self.chars_size = len(chars_set)
+
+        self._init_weight();
+
         self.data = data
 
-        self._init_weight()
 
     def train(self, batch_size, epochs):
-        """train RNN
-        """
-        print("---training start---")
-        iterations = self.data_size//batch_size 
+        """training model with SGD"""
+        print("---training---")
+
+        iterations = self.data_size//batch_size
+        hprev = np.zeros((self.hidden_size, 1))
 
         for e in range(epochs):
-            print("---epochs %d start---" % e)
-            hprev = np.zeros_like(self.hprev)
             for i in range(iterations):
-                batch_data = self.data[i*batch_size:(i+1)*batch_size]
-                batch_ch_id = self._chars_to_index(batch_data)
-                batch_target = self.data[i*batch_size+1:(i+1)*batch_size+1]
-                batch_target_ch_id = self._chars_to_index(batch_target)
+                # generate input data and label with batch_size
+                b_data = self.data[i*batch_size:(i+1)*batch_size]
+                b_data_ch_id = self._chars_to_index(b_data)
 
-                x_state, h_state, y_state, predict_state = {}, {}, {}, {}
-                print(hprev)
-                h_state[-1] = np.copy(hprev)
-                loss = 0
+                b_label = self.data[i*batch_size+1:(i+1)*batch_size+1]
+                b_label_ch_id = self._chars_to_index(b_label)
 
-                # forward pass
-                for b in range(batch_size-1):
-                    x_state[b] = np.zeros((self.vocab_size, 1))
-                    x_state[b][batch_ch_id[b]] = 1
+                state_cache, loss = self._forward(batch_size, hprev, b_data_ch_id, b_label_ch_id)
+                
+                if i % 100 == 0:
+                    print("Epoach %d Iteration %d -----Loss %d" % (e, i, loss))
 
-                    h_state[b] = np.tanh(np.dot(self.Wxh, x_state[b])+np.dot(self.Whh, h_state[b-1])+self.bh)
-                    y_state[b] = np.dot(self.Why, h_state[b]) + self.by
+                dWxh, dWhh, dbh, dWhy, dby = self._backward(batch_size, state_cache, b_label_ch_id)
 
-                    predict_state[b] = np.exp(y_state[b]) / np.sum(np.exp(y_state[b]))
-                    loss += -np.log(predict_state[b][batch_target_ch_id[b]]) 
-                    dWxh, dWhh, dWhy = np.zeros_like(self.Wxh), np.zeros_like(self.Whh), np.zeros_like(self.Why)
-                dbh, dby = np.zeros_like(self.bh), np.zeros_like(self.by)
-                dhnext = np.zeros((self.hidden_size, 1))
-
-                for b in reversed(range(batch_size-1)):
-                    # Softmax gradient, see https://raw.githubusercontent.com/eric8607242/cs231n_2017_assignment/master/assignment1/softmax_gradient.jpeg to know more detail
-                    dloss = np.copy(predict_state[b])
-                    dloss[batch_target_ch_id[b]] += -1
-
-                    dWhy += np.dot(dloss, h_state[b].T)
-                    dby += dloss 
-
-                    dh = np.dot(self.Why.T, dloss) + dhnext
-
-                    # dtanh(x)/dx = sech(x)^2
-                    # tanh^2(x) + sech^2(x) = 1
-                    # dtanh = 1 - tanh^2(x)
-                    dtanh = (1 - h_state[b] * h_state[b]) * dh
-
-                    dWhh += np.dot(dtanh, h_state[b-1].T)
-                    dbh += dtanh
-
-                    dWxh += np.dot(dtanh, x_state[b].T)
-                    dhnext = np.dot(self.Whh.T, dtanh)
-
-                for dparam in [dWxh, dWhh, dWhy, dbh, dby, h_state[len(batch_data)-2]]:
+                hprev = state_cache[1][batch_size-1]
+                for dparam in [dWxh, dWhh, dbh, dWhy, dby]:
                     np.clip(dparam, -1, 1, out=dparam)
                 
-                dparam_list = [dWxh, dWhh, dWhy, dbh, dby]
-                hprev = h_state[len(batch_data)-2]
-                self._update_weight(dparam_list)
+                self._update_weight([dWxh, dWhh, dbh, dWhy, dby])
+
             self.hprev = hprev
-                 
+            self.predict(165, self.data[0])
+        
+
+    def _forward(self, batch_size, hprev, b_data_ch_id, b_label_ch_id):
+        """forwarding
+        Args:
+            hprev(ndarray):the previous h_state in the last training step 
+            b_data_ch_id(list): the index of the batch data in char set of the input data
+            b_label_ch_id(list): the index of the batch label in char set of the input data
+
+        Return:
+            x_state(dict):the each char state in batch data
+            h_state(dict):the each h state with each of the batch data
+            y_state(dict):the each y state with each of the batch data
+            predice_state(dict):the each predict result with each of the batch data
+            loss(ndarray):the total loss for this forwarding
+        """
+        x_state, h_state, y_state, predict_state = {}, {}, {}, {}
+        h_state[-1] = hprev.copy()
+        loss = 0
+
+        for b in range(batch_size):
+            hprev = h_state[b-1]
+            # generate input data matrix
+            x_state[b] = np.zeros((self.chars_size, 1))
+            x_state[b][b_data_ch_id[b]] = 1
+
+            h_state[b] = np.tanh(np.dot(self.Wxh, x_state[b])+np.dot(self.Whh, hprev))+self.bh
+            y_state[b] = np.dot(self.Why, h_state[b])+self.by
+            predict_state[b] = np.exp(y_state[b]) / np.sum(np.exp(y_state[b]))
+            loss += -np.log(predict_state[b][b_label_ch_id[b]])
+
+        return (x_state, h_state, y_state, predict_state),  loss
+
+    def _backward(self, batch_size, state_cache, b_label_ch_id):
+        """backwarding
+
+        Args:
+            state_cache(tuple):all state in the forwarding step
+            b_label_ch_id(list):the index of the batch label in char set of the input data
+
+        Returns:
+            (ndarray):the gradient of each weight
+
+        """
+        x_state, h_state, y_state, predict_state = state_cache
+
+        dWxh, dWhh, dWhy = (
+               np.zeros_like(self.Wxh),
+               np.zeros_like(self.Whh),
+               np.zeros_like(self.Why)
+            ) 
+        dbh, dby = np.zeros_like(self.bh), np.zeros_like(self.by)
+        dhnext = np.zeros((self.hidden_size, 1))
+
+        for b in reversed(range(batch_size)):
+            # if you want to know why the gradient of Softmax is.
+            # you can check https://reurl.cc/Gpr7y
+            dloss = np.copy(predict_state[b])
+            dloss[b_label_ch_id[b]] += -1
+
+            dWhy += np.dot(dloss, h_state[b].T)
+            dby += dloss
+
+            dh_state = np.dot(self.Why.T, dloss) + dhnext
+
+            # dtanh(x)/dx = secch(x)^2
+            # tanh^2(x) + sech^2(x) = 1
+            # dtanh = 1 - tanh^2(x)
+            dtanh = (1-h_state[b] * h_state[b]) * dh_state
+            
+            dWhh += np.dot(dtanh, h_state[b-1].T)
+            dbh += dtanh
+
+            dWxh += np.dot(dtanh, x_state[b].T)
+            dhnext = np.dot(self.Whh.T, dtanh)
+        
+        return dWxh, dWhh, dbh, dWhy, dby
+            
     def _update_weight(self, dparam_list):
+        """use SGD to update weight with gradient of each weight
 
-        beta1, beta2, eps = 0.9, 0.999, 1e-8
-        param_list = [self.Wxh, self.Whh, self.Why, self.bh, self.by]
-        mparam_list = [self.mWxh, self.mWhh, self.mWhy, self.mbh, self.mby]
-        vparam_list = [self.vWxh, self.vWhh, self.vWhy, self.vbh, self.vby]
-
-        for param, dparam, mem, v in zip(param_list, dparam_list, mparam_list, vparam_list):
-            mem += beta1 * mem + (1-beta1)*dparam
-            v = beta2 * v + (1-beta2)*dparam*dparam
-            param += -self.learning_rate * mem / (np.sqrt(v+eps))
-                    
+        Args:
+            dparam_list(list): the list that contains the gradient of each weight
+        """
+        param_list = [self.Wxh, self.Whh, self.bh, self.Why, self.by]
+        
+        for param, dparam in zip(param_list, dparam_list):
+            param += -self.learning_rate * dparam 
+            
     def _chars_to_index(self, chars):
-        return [self.chars_dict[ch] for ch in chars]
+        """convert a string of characters to the index in the character set
+
+        Args:
+            chars(string):the string will be convert to index
+
+        Returns:
+            (list):the list that each character in chars is mapped to the index in character set
+        """
+        return [self.char_to_index[ch] for ch in chars]
 
     def _index_to_chars(self, index):
-        return [self.index_dict[i] for i in index]
+        """convert a list of integer which is the index of the character to the character in the character set
 
+        Args:
+            index(list):the integer list will be convert to character
+
+        Returns:
+            (list):the list that each integer in index is mapped to the character in character set
+        """
+        return [self.index_to_char[i] for i in index]
+    
     def predict(self, seq_len, input_data):
-        x = np.zeros((self.vocab_size, 1))
+        """predict the sequence 
+
+        Args:
+            seq_len(int): the length that predict string will be
+            input_data(char): the first char to feed into RNN model
+        """
+        # generate the input data matrix
+        x = np.zeros((self.chars_size, 1))
         x_id = self._chars_to_index(input_data)
         x[x_id] = 1
-        h = np.copy(self.hprev)
+        h_state = self.hprev        
 
         seq_index = []
         for t in range(seq_len):
-            h = np.tanh(np.dot(self.Wxh, x) + np.dot(self.Whh, h) + self.bh) 
-            y = np.dot(self.Why, h) + self.by
-            p = np.exp(y) / np.sum(np.exp(y))
-            
-            x_id = np.argmax(p)
-            x = np.zeros((self.vocab_size, 1))
+            h_state = np.tanh(np.dot(self.Wxh, x)+np.dot(self.Whh, h_state)) + self.bh
+            y_state = np.dot(self.Why, h_state) + self.by
+            p_state = np.exp(y_state)/np.sum(np.exp(y_state))
+
+            x_id = np.argmax(p_state)
+            x = np.zeros((self.chars_size, 1))
             x[x_id] = 1
             seq_index.append(x_id)
 
         output_seq = ''.join(self._index_to_chars(seq_index))
         print(output_seq)
-            
 
-if __name__ == '__main__' :
-    rnn = RNN(100, 1e-3, 'input.txt')
-    rnn.train(3, 2)
-    rnn.predict(10, "h")
+if __name__=='__main__':
+    rnn = RNN(200, 3e-3);
+    rnn.load_data('input.txt')
+    rnn.train(165,50)
